9d0d1b7260b68bb914a5c9256dd76ca52a2ee8f6
dijx <dijxie@gmail.com>
2019-03-29 19:35:17 +0100

    Initial commit

---
+++ src/com/remik/PrimeNumbers.java
@@ -0,0 +1,27 @@
+package com.remik;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i+" : "+isPrime(i));
+        }
+    }
+
+
+     static boolean isPrime(int i) {
+        boolean ret=true;
+        if (i < 2) {
+            ret = false;
+        } else {
+            int limit = i / 2;
+            for (int j = 2; j <= limit; j++) {
+                if (i % j == 0) {
+                    ret = false;
+                    break;
+                }
+            }
+        }
+         return ret;
+    }
+}
